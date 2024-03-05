5dfa82d59e54540e9208067f9809a8fbf86772c0
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-03-05 10:18:28 -0800

    center and ljust methods

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,6 +1,7 @@
-name = "Finn"
+name = "finn"
 health = 60
 
 puts "Let's play a game!\n\n\t3\n\t2\n\t1\n\n"
 puts "*" * 25
-puts "#{name}'s health is #{health / 9.0}"
+puts "#{name.capitalize}'s health is #{health}".center(50, '*')
+puts "#{name.capitalize.ljust(30, ".")}#{health}health" 
