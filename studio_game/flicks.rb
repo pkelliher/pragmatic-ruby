f9d0d49ed127be1ad67acb7d1ab2e7a9f2f0f7ce
Patrick Kelliher <patrick.kelliher@greenhouse.io>
2024-03-25 18:47:15 -0700

    Array methods

--- studio_game/flicks.rb
+++ studio_game/flicks.rb
@@ -21,35 +21,28 @@ class Movie
 end
 
 movie_1 = Movie.new("goonies", 10)
+movie_2 = Movie.new("ghostbusters", 9)
+movie_3 = Movie.new("goldfinger")
 
-number_rolled = rand(1..6)
+movies = [movie_1, movie_2, movie_3]
 
-case number_rolled
-when 1..2
-  movie_1.thunmbs_down
-  puts "#{movie_1.title} got a thumbs down."
-when 3..4
-  puts "#{movie_1.title} was skipped."
-else
-  movie_1.thunmbs_up
-  puts "#{movie_1.title} got a thumbs up."
+puts "\nBefore watching:"
+puts movies
+
+movies.each do |movie|
+  number_rolled = rand(1..6)
+
+  case number_rolled
+  when 1..2
+    movie_1.thunmbs_down
+    puts "#{movie.title} got a thumbs down."
+  when 3..4
+    puts "#{movie.title} was skipped."
+  else
+    movie_1.thunmbs_up
+    puts "#{movie.title} got a thumbs up."
+  end
 end
 
-# puts movie_1.title
-# movie_1.title = "goonyes"
-# puts movie_1.title
-# puts movie_1.rank
-
-# puts movie_1
-# movie_1.thunmbs_up
-# puts movie_1
-# movie_1.thunmbs_up
-# puts movie_1
-
-# movie_2 = Movie.new("ghostbusters", 9)
-# puts movie_2
-# movie_2.thunmbs_down
-# puts movie_2
-
-# movie_3 = Movie.new("goldfinger")
-# puts movie_3
+puts "\nAfter watching:"
+puts movies
--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -2,42 +2,63 @@ class Player
   attr_accessor :name
   attr_reader :health
   
-  def score
-    @health + @name.length
-  end
-
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
   end
 
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{@score}"
+  def drain
+    @health -= 10
   end
 
   def boost
     @health += 15
   end
 
-  def drain
-    @health -= 10
+  def score
+    @health + @name.length
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}"
   end
 end
 
 player_1 = Player.new("finn", 60)
+player_2 = Player.new("lucy", 90)
+player_3 = Player.new("jase")
+player_4 = Player.new("alex", 125)
 
-number_rolled = rand(1..6)
+players = [player_1, player_2, player_3, player_4]
 
-case number_rolled
-when 1..2
-  player_1.drain
-  puts "#{player_1.name} got drained."
-when 3..4
-  puts "#{player_1.name} was skipped."
-else
-  player_1.boost
-  puts "#{player_1.name} got a boost."
+players.pop
+player_5 = Player.new("cole", 75)
+players.push(player_5)
+
+puts "\nBefore playing:"
+puts players
+
+
+
+
+players.each do |player|
+  number_rolled = rand(1..6)
+
+  case number_rolled
+  when 1..2
+    player.drain
+    puts "#{player.name} got drained 😩"
+  when 3..4
+    puts "#{player.name} got skipped"
+  else
+    player.boost
+    puts "#{player.name} got boosted 😁"
+  end
 end
+
+puts "\nAfter playing:"
+puts players
+
 # puts player_1
 # puts player_1.health
 
